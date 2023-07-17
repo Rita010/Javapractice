859bd45eae348c32b2ff4afa520d73f0d4e55199
Rita010 <ritakhatiwada50@gmail.com>
2023-07-17 12:13:36 -0700

    second

---
+++ Monday.java
@@ -0,0 +1,27 @@
+package revisionclassjune27;
+
+public class Monday {
+
+	public static void main(String[] args) {
+	
+		sub (2,3);
+		sub (2,3,4);
+		sub (2,3,4,5);
+		
+		
+		
+		
+	}
+public static void sub (int a, int b) {
+			System.out.println(a+b);
+}
+			public static void sub (int a, int b, int c) {
+				
+				System.out.println(a+b+c);
+					
+				}
+				public static void sub (int a, int b, int c, int d) {
+					System.out.println(a+b+c+d);
+			}
+
+}
