46b507e34a7bdd9b5313c6f8fc51b722ad76092c
Rita010 <ritakhatiwada50@gmail.com>
2023-07-17 14:00:41 -0700

    third

---
+++ Monday1.java
@@ -0,0 +1,21 @@
+package revisionclassjune27;
+
+public class Monday1 {
+
+	public static void main(String[] args) {
+
+		int sum = add (4,5);
+		System.out.println(sum);
+		System.out.println(sum + sum);
+		System.out.println(sum - 5);
+		System.out.println(sum * 0.01);
+		
+		
+		
+	}
+
+	public static int add (int x, int y) {
+		return x+y;
+		
+	}
+}
